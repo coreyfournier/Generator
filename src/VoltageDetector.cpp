3c166a789e275f18b257bd5bfc9d7dba4ec7aed9
corey.fournier@gmail.com <corey.fournier@gmail.com>
2023-09-19 18:26:20 -0500

    Started working on code to read the voltage

---
+++ src/VoltageDetector.cpp
@@ -0,0 +1,16 @@
+#include <stdint.h>
+
+using namespace std;
+
+class VoltageDector
+{
+
+    public:
+    /**
+     * Parameter is the function to read the analog pin
+    */
+    VoltageDector(int (*func)(uint8_t), uint8_t gpioPin){
+
+    }
+
+};
